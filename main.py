d64723b7d5cdf6b8115459cc7562c726ebd6c8ad
Royal51651 <georgeeggers682@gmail.com>
2025-05-06 17:26:51 -0400

    Added setup.py to intialize the project, and main.py to run the server itself

--- main.py
+++ main.py
@@ -1,52 +1,19 @@
-import os
-import platform
 import subprocess
+import json
+print("Fira Server Launcher")
+try:
+    with open('config.json') as f:
+        settings = json.load(f)
+except:
+    print("Malformed config.json. Run setup.py again to fix")
+    exit()
 
-print("Fira server launcher")
-print("Is port forwarding currently enabled on your network, and pointing to your machine (yes / no)")
-while(True):
-    choice = input(": ")
-    if(choice == "yes"):
-        break
-    elif(choice == "no"):
-        print("Make sure that is set up before tyring to make this publically accessible")
-        print("Pocketbase will now serve on the default localhost address (127.0.0.1:8090)")
-        break
-    else:
-        print("Invalid choice. Try again")
-operating_sys = platform.system()
-architecture = platform.processor()
-print(f"Detected Operating System: {operating_sys}")
-print(f"Detected architecture: {platform.processor()}")
-correct = True
-while(True):
-    print("Is this right? (yes / no)")
-    choice = input(": ")
-    if(choice == "yes"):
-        break
-    elif(choice == "no"):
-        correct = False
-        break
-    else:
-        print("Invalid choice. Try again")
 
-if(not correct):
-    while(True):
-        print("New operating system")
-        print("(Windows, Darwin, Linux)")
-        operating_sys = input(": ")
-        if(operating_sys in ["Windows", "Darwin", "Linux"]):
-            break
-        else:
-            print("Invalid choice. Try again")
-    while(True):
-        print("New architecture")
-        print("(amd, x86_64)")
-        architecture = input(": ")
-        if(architecture in ["amd", "x86_64"]):
-            break
-        else:
-            print("Invalid choice. Try again")
-print(f"Running pocketbase for {operating_sys} on {architecture}")
-print("Press ^ + c to stop, or close terminal window")
-process = subprocess.run([f"./pocketbase-{operating_sys}-{architecture}", "serve"])
+operating_sys = settings["os"]
+architecture = settings["architecture"]
+if(settings["extracted"]):
+    print(f"Running pocketbase for {operating_sys} on {architecture}")
+    print("Press ^ + c to stop, or close terminal window")
+    process = subprocess.run([f"./pocketbase-{operating_sys}-{architecture}", "serve"])
+else:
+    print("Pocketbase not extracted yet. Re-run setup.py to fix")
---
+++ setup.py
@@ -0,0 +1,78 @@
+import platform
+import json
+import zipfile
+import shutil
+data = {
+    "os": "",
+    "architecture": "",
+    "extracted": False
+}
+print("Fira Server Setup")
+print("Is port forwarding currently enabled on your network, and pointing to your machine (yes / no)")
+while(True):
+    choice = input(": ")
+    if(choice == "yes"):
+        break
+    elif(choice == "no"):
+        print("Make sure that is set up before tyring to make this publically accessible")
+        print("Pocketbase will serve on a default localhost address instead (127.0.0.1:8090)")
+        break
+    else:
+        print("Invalid choice. Try again")
+operating_sys = platform.system()
+print(f"Detected Operating System: {operating_sys}")
+correct = True
+while(True):
+    print("Is this right?\n(yes / no)")
+    choice = input(": ")
+    if(choice == "yes"):
+        break
+    elif(choice == "no"):
+        correct = False
+        break
+    else:
+        print("Invalid choice. Try again")
+
+if(not correct):
+    while(True):
+        print("Select operating system\n(Windows, Darwin, Linux)")
+        operating_sys = input(": ")
+        if(operating_sys in ["Windows", "Darwin", "Linux"]):
+            break
+        else:
+            print("Invalid choice. Try again")
+while(True):
+    print("Select CPU architecture\n(arm, x86_64)")
+    architecture = input(": ")
+    if(architecture in ["arm", "x86_64"]):
+        break
+    else:
+        print("Invalid choice. Try again")
+
+data["os"] = operating_sys
+data["architecture"] = architecture
+print("Attempting to extract the proper version of pocketbase")
+with zipfile.ZipFile(f"zips/pocketbase-{operating_sys}-{architecture}.zip", 'r') as zip:
+    zip.extractall()
+print("Pocketbase Extracted")
+data["extracted"] = True
+print("Delete unused pocketbase versions?\n(yes / no)")
+while(True):
+    choice = input(": ")
+    if(choice == "yes"):
+        print("Deleting files")
+        shutil.rmtree("zips")
+        break
+    else:
+        break
+
+print("Saving data to config file")
+conf_data = json.dumps(data, indent=2)
+
+with open("config.json", "w") as f:
+    f.write(conf_data)
+
+print("Data saved! Run main.py to begin")
+    
+
+
